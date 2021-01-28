f7897877adefcd7053697422fc26ec44379cf7ce
Igor Almeida <igoralmeeida@hotmail.com>
2021-01-28 18:59:44 -0300

    creating controllers Edital e Bolsa

---
+++ app/Http/Controllers/BolsaController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BolsaController extends Controller
+{
+    public function listarBolsaBeneficiario(){
+
+    }
+
+    public function listarBolsasPrograma(){
+
+    }
+
+    public function gerarBolsas(){
+
+    }
+
+    public function editarBolsa(){
+
+    }
+
+
+}
---
+++ app/Http/Controllers/EditalController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EditalController extends Controller
+{
+    public function cadastrarEdital(){
+
+    }
+
+    public function editarEdital(){
+
+    }
+
+    public function listarEdital(){
+
+    }
+
+    public function deletarEdital(){
+
+    }
+}
