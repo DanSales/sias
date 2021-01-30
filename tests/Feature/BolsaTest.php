2d9081a4d584c191d01c326d0c9e309bc6293a34
Igor Almeida <igoralmeeida@hotmail.com>
2021-01-30 07:42:09 -0300

    tests de bolsa e conta

--- tests/Feature/BolsaTest.php
+++ tests/Feature/BolsaTest.php
@@ -2,9 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Models\Conta;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Beneficiario;
+use App\Models\User;
+
 
 class BolsaTest extends TestCase
 {
@@ -15,7 +19,27 @@ class BolsaTest extends TestCase
      */
 
     public function testListBolsaOtherBeneficiario(){
+        $beneficiario1 = Beneficiario::factory()->create();
+        $beneficiario2 = Beneficiario::factory()->create();
+        $user2 = User::find($beneficiario2->user_id);
 
+        $conta = Conta::factory()->create(['beneficiario_id' => $beneficiario1->id]);
+
+        $this->actingAs($user2)
+            ->get('/contas/'.$conta->id.'/bolsas')
+            ->assertStatus(403);
+    }
+
+    public function testListBolsasBeneficiario(){
+        $beneficiario1 = Beneficiario::factory()->create();
+
+        $conta = Conta::factory()->create(['beneficiario_id' => $beneficiario1->id]);
+
+        $user2 = User::find($beneficiario1->user_id);
+
+        $this->actingAs($user2)
+            ->get('/contas/'.$conta->id.'/bolsas')
+            ->assertStatus(200);
     }
 
 
--- tests/Feature/ContaTest.php
+++ tests/Feature/ContaTest.php
@@ -2,9 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\Models\Servidor;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Beneficiario;
+use App\Models\Candidato;
+use App\Models\Conta;
+use Illuminate\Support\Facades\Session;
 
 class ContaTest extends TestCase
 {
@@ -14,15 +20,111 @@ class ContaTest extends TestCase
      * @return void
      */
 
-    public function testListConta(){
+    public function testListContaSucesso(){
+        $beneficiario1 = Beneficiario::factory()->create();
+        $user2 = User::find($beneficiario1->user_id);
 
+        $conta = Conta::factory()->create(['beneficiario_id' => $beneficiario1->id]);
+
+        $this->actingAs($user2)
+            ->get('/contas/')
+            ->assertStatus(200);
+    }
+
+    public function testErroListConta(){
+        $candidato = Candidato::factory()->create();
+        $user2 = User::find($candidato->user_id);
+
+        $this->actingAs($user2)
+            ->get('/contas/')
+            ->assertStatus(403);
     }
 
     public function testCreateConta(){
+        $beneficiario = Beneficiario::factory()->create();
+        $user = User::find($beneficiario->user_id);
 
+        Session::start();
+
+        $credentials = array(
+            'username' => $user->email,
+            'password' => 'Testing.1234',
+            '_token' => csrf_token()
+        );
+
+        $this->call('POST', '/login', $credentials);
+
+        $conta = ['agencia' => '0011',
+                    'banco' => 'teste',
+                    'codigo_banco' => '001',
+                    'tipo_conta' => 'CC',
+                    'numero_conta' => '1010',
+                    'ativa' => false,
+                    'beneficiario_id' => $beneficiario->id,
+                    '_token' => csrf_token()];
+
+        $this->actingAs($user)
+            ->post('/contas/adicionar',$conta)
+            ->assertLocation('/contas');
     }
 
-    public function testUpdateConta(){
+    public function testCadidatoCreateConta(){
 
+        $candidato = Candidato::factory()->create();
+        $user = User::find($candidato->user_id);
+
+        Session::start();
+
+        $credentials = array(
+            'username' => $user->email,
+            'password' => 'Testing.1234',
+            '_token' => csrf_token()
+        );
+
+        $this->call('POST', '/login', $credentials);
+
+        $conta = ['agencia' => '0011',
+            'banco' => 'teste',
+            'codigo_banco' => '001',
+            'tipo_conta' => 'CC',
+            'numero_conta' => '1010',
+            'ativa' => false,
+            'beneficiario_id' => $candidato->id,
+            '_token' => csrf_token()];
+
+        //dd($conta);
+
+        $this->actingAs($user)
+            ->post('/contas/adicionar',$conta)
+            ->assertStatus(403);
     }
+
+    public function testCreateContaSemFilds(){
+        $beneficiario = Beneficiario::factory()->create();
+        $user = User::find($beneficiario->user_id);
+
+        Session::start();
+
+        $credentials = array(
+            'username' => $user->email,
+            'password' => 'Testing.1234',
+            '_token' => csrf_token()
+        );
+
+        $this->call('POST', '/login', $credentials);
+
+        $conta = ['agencia' => '',
+            'banco' => '',
+            'codigo_banco' => '001',
+            'tipo_conta' => 'CC',
+            'numero_conta' => '1010',
+            'ativa' => false,
+            'beneficiario_id' => $beneficiario->id,
+            '_token' => csrf_token()];
+
+        $this->actingAs($user)
+            ->post('/contas/adicionar',$conta)
+            ->assertLocation('/contas/adicionar');
+    }
+
 }
