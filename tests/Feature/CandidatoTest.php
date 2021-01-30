0006d11283756a1989a668a6c8a5f84147c80383
Igor Almeida <igoralmeeida@hotmail.com>
2021-01-30 08:30:08 -0300

    candidato test

--- tests/Feature/CandidatoTest.php
+++ tests/Feature/CandidatoTest.php
@@ -4,7 +4,10 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Faker;
 use Tests\TestCase;
+use App\Models\Candidato;
+use App\Models\User;
 
 class CandidatoTest extends TestCase
 {
@@ -14,15 +17,32 @@ class CandidatoTest extends TestCase
      * @return void
      */
 
-    public function testCandidatoLogadoViewBolsa(){
+    public function testCandidatoErroRegister(){
+        $faker = Faker\Factory::create();
+        $this->get('/register');
+        $form = [
+            '_token' => csrf_token(),
+            'nome_completo' => '',
+            'cpf' => $faker->numberBetween(10000000000,99999999999),
+            'data_nascimento' => $faker->date('d/m/yy'),
+            'endereco' => $faker->streetAddress,
+            'email' => $faker->email,
+            'password' => 'Testing.1234',
+            'password-confirm' =>'Testing.1234',
+        ];
 
-    }
 
-    public function testCandidatoRegister(){
+        $response = $this->post('/register', $form)
+            ->assertLocation('/register');
 
     }
 
     public function testCandidadoViewPrograma(){
+        $candidato = Candidato::factory()->create();
+        $user = User::find($candidato->user_id);
 
+        $this->actingAs($user)
+            ->get('/programa')
+            ->assertStatus(200);
     }
 }
