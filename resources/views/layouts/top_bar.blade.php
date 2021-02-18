c48222f851a73a6c53962dd1a1a2a313f62c0856
Igor Almeida <igoralmeeida@hotmail.com>
2021-02-18 14:24:31 -0300

    new dropDown

--- resources/views/layouts/top_bar.blade.php
+++ resources/views/layouts/top_bar.blade.php
@@ -174,20 +174,11 @@
                 @auth
                 <a class="dropdown-item" href="{{route('atualizarUserView')}}">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Profile
+                    Perfil
                 </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Settings
-                </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Activity Log
-                </a>
-                <div class="dropdown-divider"></div>
                 @endauth
                 @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    <div class="hidden fixed top-0 right-0 px-6 sm:block">
                         @auth
                             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
